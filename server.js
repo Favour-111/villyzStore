8edee2b16e2b8ac9ccf4805ba19e94ead80c333c
favour <omojolaobaloluwa@gmail.com>
2025-04-05 18:41:52 +0100

    done

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ mongoose
   });
 //Port
 app.use(express.json());
-app.use("/api/users/webhook", express.raw({ type: "application/json" })); // BEFORE app.use(express.json())
 app.use(cors());
 app.use(AdminRoute);
 app.use(Route);
